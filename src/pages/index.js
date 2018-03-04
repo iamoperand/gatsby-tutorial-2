ee58cbb3c21f22504c14a495b1da74fdce17e9de
Nikhil Arora <narora200@gmail.com>
2018-03-04 12:43:23 +0530

    integrated styled-components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React from "react"
+import Link from 'gatsby-link'
 
 export default () =>
     <div style={{ margin: '3rem auto', maxWidth: 600 }}>
@@ -23,5 +24,6 @@ export default () =>
                 </blockquote>
             </div>
             <p>Posted April 09, 2011</p>
+            <Link to="/styled-page/">Go to Styled Page</Link>
         </div>
     </div>
